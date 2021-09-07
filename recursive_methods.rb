de31eb724302c65f3d19d20f72b6f524b0f93f34
parentda <danielparent21@gmail.com>
2021-09-07 13:11:11 -0400

    added recursive bottles_of_beer method

--- recursive_methods.rb
+++ recursive_methods.rb
@@ -1,11 +1,20 @@
-def factorial(n)
-  return 1 if n == 1
+def factorial(num)
+  return 1 if num == 1
 
-  n * factorial(n - 1)
+  num * factorial(num - 1)
 end
 
 def palindrome?(string)
   return true if string.length <= 1
 
   string[0] == string[-1] ? palindrome?(string[1...-1]) : (return false)
+end
+
+def bottles_of_beer(num)
+  if num.zero?
+    puts 'no more bottles of beer on the wall'
+  else
+    puts "#{num} bottles of beer on the wall"
+    bottles_of_beer(num - 1)
+  end
 end
